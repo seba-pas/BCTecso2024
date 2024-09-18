ade4dae91b4bfeb4d9bb5f076547474930d37e05
Sebastian Pastorenzi <sebastian.pastorenzi@tecso.coop>
2024-09-18 14:50:50 -0300

    prueba axios

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,8 @@
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { Form, Button, Container } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
@@ -13,100 +14,105 @@ const validationSchema = Yup.object().shape({
 });
 
 function Login() {
-    const navigate = useNavigate();
-
+  // const navigate = useNavigate();
 
   const handleSubmit = (values) => {
     console.log("Email:", values.email);
     console.log("Password:", values.password);
-    if(values.email === "admin@admin.com" && values.password === "admin123"){
-        navigate("/home");
+    axios
+      .post("http://localhost:8080/api/Authentication/token", {
+        user: values.email,
+        password: values.password,
+      })
+      .then((response) => {
+        console.log("Response:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 
-    }
+    // navigate("/home");
   };
 
   return (
-    <div className='bg-dark vh-100 justify-content-center align-items-center'>
-
-
-
-    <Container
-      className="bg-dark text-white rounded-2 py-4 align-self-center"
-      style={{
-        maxWidth: "400px",
-        paddingTop: "50px",
-        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <h2>Login</h2>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          handleSubmit(values);
-          setSubmitting(false);
+    <div className="bg-dark vh-100 justify-content-center align-items-center">
+      <Container
+        className="bg-dark text-white rounded-2 py-4 align-self-center"
+        style={{
+          maxWidth: "400px",
+          paddingTop: "50px",
+          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
         }}
       >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                name="email"
-                placeholder="Enter email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                isInvalid={!!errors.email && touched.email}
-              />
-              {errors.email && touched.email && (
-                <Form.Control.Feedback type="invalid">
-                  {errors.email}
-                </Form.Control.Feedback>
-              )}
-            </Form.Group>
+        <h2>Login</h2>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            handleSubmit(values);
+            setSubmitting(false);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={!!errors.email && touched.email}
+                />
+                {errors.email && touched.email && (
+                  <Form.Control.Feedback type="invalid">
+                    {errors.email}
+                  </Form.Control.Feedback>
+                )}
+              </Form.Group>
 
-            <Form.Group controlId="formBasicPassword" className="mt-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                isInvalid={!!errors.password && touched.password}
-              />
-              {errors.password && touched.password && (
-                <Form.Control.Feedback type="invalid">
-                  {errors.password}
-                </Form.Control.Feedback>
-              )}
-            </Form.Group>
+              <Form.Group controlId="formBasicPassword" className="mt-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={!!errors.password && touched.password}
+                />
+                {errors.password && touched.password && (
+                  <Form.Control.Feedback type="invalid">
+                    {errors.password}
+                  </Form.Control.Feedback>
+                )}
+              </Form.Group>
 
-            <div className="justify-content-center w-100">
-              <Button
-                variant="primary"
-                type="submit"
-                className="mt-4 align-self-center"
-                disabled={isSubmitting}
-              >
-                Login
-              </Button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </Container>
+              <div className="justify-content-center w-100">
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="mt-4 align-self-center"
+                  disabled={isSubmitting}
+                >
+                  Login
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </Container>
     </div>
   );
 }
