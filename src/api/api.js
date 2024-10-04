863945fd84bddbf4cc0032e83377ed663401cf3e
Leandro Florentin <102199433+LeandroFlorentin@users.noreply.github.com>
2024-10-04 16:23:58 -0300

    rebase

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const { VITE_API_URL_GENERAL } = import.meta.env;
+import { Resend } from "resend";
+import emailjs from "@emailjs/browser";
+import sgMail from "@sendgrid/mail";
+const { VITE_API_URL_GENERAL, VITE_API_API_KEY_RESEND, VITE_API_EMAIL_MUMA } = import.meta.env;
 const instance = axios.create({
   baseURL: VITE_API_URL_GENERAL,
   timeout: 3000,
@@ -21,5 +24,42 @@ export const PostGeneral = async (path, body) => {
     return respuesta.data;
   } catch (error) {
     throw error;
+  }
+};
+
+export const RegisterPet = async (path, body) => {
+  try {
+    const response = await instance.post(path, body);
+    return response.data;
+  } catch (error) {
+    throw error.response.data.errors[0];
+  }
+};
+//emailjs
+/* export const SendEmail = async (form) => {
+  try {
+    let response = await emailjs.sendForm("service_mtwmyww", "template_3h5r5hq", form, "0ezY2pVr11nIQIT_8");
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}; */
+//resend
+export const SendEmail = async (emailto, form) => {
+  try {
+    const response = await fetch("/netlify/functions/sendEmail.js", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ emailto, form }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to send email");
+    }
+
+    const result = await response.json();
+    console.log(result.message);
+  } catch (error) {
+    console.error("Error:", JSON.stringify(error));
   }
 };
