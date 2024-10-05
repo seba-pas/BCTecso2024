3cd51cc066c62bceaa56dc6d83a7ee6322972941
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-10-05 17:04:33 -0300

    registro-de-protectora: añadi peticiones ( aun no completado )

--- src/api/setupAxios.js
+++ src/api/setupAxios.js
@@ -24,22 +24,79 @@ export const PostGeneral = async (path, body) => {
   }
 };
 
-
-
-//Configuracion axios login. 
-export const login = async(values) =>{
+//Configuracion axios login.
+export const login = async (values) => {
   try {
-    const response = await instance.post(`Authentication/token`,{
-        user : values.email,
-        password: values.password
+    const response = await instance.post(`Authentication/token`, {
+      user: values.email,
+      password: values.password,
     });
-    if(response.data && response.data.token){
+    if (response.data && response.data.token) {
       const token = response.data.token;
-      localStorage.setItem('token', token);
-    }else{
-      throw new Error('No se recibio el token')
+      localStorage.setItem("token", token);
+    } else {
+      throw new Error("No se recibio el token");
     }
   } catch (error) {
-    throw(error);
+    throw error;
   }
-}
+};
+
+export const registerNewShelter = async (values) => {
+  // Preparar los datos para el backend
+  delete values.password2;
+  const { idCiudad, calle, numero, piso, departamento, password1: password, ...rest } = values;
+
+  const dataToSend = {
+    ...rest,
+    password,
+    direccion: {
+      idCiudad: idCiudad,
+      calle: calle,
+      numero: numero,
+      piso: piso,
+      departamento: departamento,
+      provincia: {
+        id: 0, //! Sacar de backend
+        nombre: "string", //! Sacar de backend
+      },
+      ciudad: {
+        id: idCiudad,
+        nombre: "string", //! Sacar de backend
+        idProvincia: 0, //! Sacar de backend
+      },
+    },
+  };
+
+  console.log(dataToSend);
+
+  try {
+    const response = await instance.post(`Protectoras/registro`, {
+      dataToSend,
+    });
+
+    console.log(response);
+  } catch (error) {
+    console.error("Error al registrar protectora:", error);
+    // Maneja el error según sea necesario
+  }
+};
+
+export const getProvinces = async () => {
+  try {
+    const response = await instance.get("Combos/Provincias");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getCities = async (province) => {
+  console.log({province});
+  
+  try {
+    const response = await instance.get(`Combos/Ciudades/${province}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
