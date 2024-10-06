e63856ce0b5dd44c3fcd8f6db0df0c855eff921d
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-10-06 01:48:42 -0300

    registro-de-protectora: cambie select por Select de react-select

--- src/RegisterShelter/components/RegisterShelterSelect.jsx
+++ src/RegisterShelter/components/RegisterShelterSelect.jsx
@@ -1,19 +1,46 @@
 import PropTypes from "prop-types";
 import { ErrorMessage, useField } from "formik";
+import Select from "react-select";
 
-export const RegisterShelterSelect = ({ label, ...props }) => {
-  const [field] = useField(props);
+import { customStyles } from "../style/selectCustomStyle";
+
+export const RegisterShelterSelect = ({ options, customOnChange, placeholder, ...props }) => {
+  const [field, meta, helpers] = useField(props);
+
+  const handleChange = (selectedOption) => {
+    helpers.setValue(selectedOption); // Establece el valor seleccionado en Formik
+    if (customOnChange) {
+      customOnChange(selectedOption);
+    }
+  };
+
+  const customNoOptionsMessage = () => <div>Seleccione una provincia</div>;
+
+  const styles = {
+    ...customStyles,
+    control: (provided) => ({
+      ...customStyles.control(provided), // Llama a tu función de estilos personalizada
+      border: meta.touched && meta.error ? "1px solid var(--system-error)" : "none",
+    }),
+  };
 
   return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <select {...field} {...props} />
-      <ErrorMessage name={props.name} component="span" />
-    </>
+    <div>
+      <Select {...field} {...props} options={options} onChange={handleChange} value={field.value || null} noOptionsMessage={customNoOptionsMessage} styles={styles} placeholder={placeholder} />
+      <ErrorMessage
+        name={props.name}
+        component="span"
+        style={{
+          color: "var(--system-error)",
+          paddingLeft: "16px",
+        }}
+      />
+    </div>
   );
 };
 
 RegisterShelterSelect.propTypes = {
-  label: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  placeholder: PropTypes.string,
+  customOnChange: PropTypes.func,
 };
