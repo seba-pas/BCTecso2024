da6957fb9d19e5844e5f81527a07d9ee6e46d6b4
HildaEncina <encina.hilda@gmail.com>
2024-10-08 19:48:49 -0300

    Se resolvio un conflicto con dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login/Login';
-import Rol from './Pages/Rol/Rol';
+
+import SelectUser from './Pages/SelectUser/SelectUser';
+// import ProtectorRegister from './Pages/ProtectorRegister';
+// import PetterRegister from './Pages/PetterRegister';
+// import RegisterRefused from './Pages/RegisterRefused';
+// import RegisterSuccess from './Pages/RegisterSuccess';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/rol" element={<Rol />} />
+
+      <Route path="/select-user" element={<SelectUser />} />
+      {/* <Route path="/register/protector" element={<ProtectorRegister />} /> */}
+      {/* <Route path="/register/petter" element={<PetterRegister />} />
+      <Route path="/register/refused" element={<RegisterRefused />} />
+      <Route path="/register/success" element={<RegisterSuccess />} /> */}
+
 
     </Routes>
   );
