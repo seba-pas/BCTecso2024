bd139988fe599547d72c2ba87b3d23b8fd48c99f
HildaEncina <encina.hilda@gmail.com>
2024-10-07 22:25:32 -0300

    feat(F04) Se creo el componente Seleccion de Rol, y se agrego estilos al diseño web

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login/Login';
+import Rol from './Pages/Rol/Rol';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/rol" element={<Rol />} />
+
     </Routes>
   );
 }
