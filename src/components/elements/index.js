3a52df90ae74273e52814761d4503d8fbb44b1b5
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-10-10 16:36:52 -0300

    detalle-de-mascota: cambio de carpetas

---
+++ src/components/elements/index.js
@@ -0,0 +1,10 @@
+export { default as Select } from "react-select/base";
+export { default as ArrowBack } from "./ArrowBack";
+export { default as Check } from "./Check";
+export { default as MyDatePicker } from "./DatePicker";
+export { EyeIcon } from "./EyeIcon";
+export { HeaderButton } from "./HeaderButton";
+export { default as Input } from "./Input";
+export { MapPin } from "./MapPin";
+export { Pill } from "./Pill";
+export { Tag } from "./Tag";
