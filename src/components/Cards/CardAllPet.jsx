b588043d7fc76e1a607a19ed2552b9a1f1754876
Juan <juan.calvo.dev@gmail.com>
2024-10-17 12:39:33 -0300

    Se agrego favoritos en allPets

--- src/components/Cards/CardAllPet.jsx
+++ src/components/Cards/CardAllPet.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import iconSex from "../../assets/images/icons/sexo.png";
+import { useDispatch,useSelector } from "react-redux";
+import { addToWishlist,removeFromWishlist } from "../../features/wishlist/whishlistSlice";
 
 const CardAllPet = ({ index, image }) => {
+
+  const dispatch = useDispatch();
+  const wishList = useSelector((state) => state.whishlist);
+  const isInWishlist = wishList.includes(image.id);
+
+  const handleWishlistClick = () => {
+    if(isInWishlist){
+      dispatch(removeFromWishlist(image.id));
+    }else{
+      dispatch(addToWishlist(image.id));
+    }
+  };
+
   const navigate = useNavigate();
   return (
-    <div key={index} className="pb-5" onClick={() => navigate(`/pet_details/${image.id}`)}>
+    <div key={index} className="pb-5" >
       <div className="card ms-3" style={{ width: "10rem", height: "16rem", border: "none", boxShadow: "-4px 14px 17px -3px rgba(0,0,0,0.25)" }}>
-        <i className="bi bi-heart fs-6 text-danger pets-wishList"></i>
-        <img src={image.fotos[0]} className="card-img-top pet-img pet-size-image pointer" alt={`image-${index}`} />
+        <i className={`bi ${isInWishlist ? "bi-heart-fill" : "bi-heart"} bi-heart fs-6 text-danger pets-wishList`} onClick={handleWishlistClick}></i>
+        <img src={image.fotos[0]} className="card-img-top pet-img pet-size-image pointer" alt={`image-${index}`} onClick={() => navigate(`/pet_details/${image.id}`)}/>
         <div className="card-body pt-2">
           <div className="d-flex justify-content-between align-items-center">
             <h5 className="card-title pet-name">{image.nombre}</h5>
